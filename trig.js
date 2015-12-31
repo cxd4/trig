04efad4ea61b16a19628b7aca79c2c62cdac21f2
no <rj150@hotmail.com>
2015-12-30 20:49:50 -0500

    uploaded basic website template with isosceles right triangle

---
+++ trig.js
@@ -0,0 +1,82 @@
+var coordinates_per_vertex = 4;
+var triangle = [];
+
+var frames_per_second = 4;
+
+function display() {
+    "use strict";
+
+    glDrawArrays(GL_LINE_LOOP, 0, 3);
+    return;
+}
+
+function init() {
+    "use strict";
+    var i, j;
+    var x, y;
+    var X = 0, Y = 1, Z = 2, W = 3;
+
+ // Default to a 45-45-90, isosceles right triangle.
+    x = 1;
+    y = 1;
+
+    triangle[0 * coordinates_per_vertex + X] = 0;
+    triangle[0 * coordinates_per_vertex + Y] = 0;
+
+    triangle[1 * coordinates_per_vertex + X] = x;
+    triangle[1 * coordinates_per_vertex + Y] = y;
+
+    triangle[2 * coordinates_per_vertex + X] = x;
+    triangle[2 * coordinates_per_vertex + Y] = 0;
+
+    for (i = 0; i < 3; i += 1) { // Adjust origin(0, 0) to (-1, -1).
+        triangle[i * coordinates_per_vertex + X] *= 2.0;
+        triangle[i * coordinates_per_vertex + Y] *= 2.0;
+        triangle[i * coordinates_per_vertex + X] -= 1.0;
+        triangle[i * coordinates_per_vertex + Y] -= 1.0;
+    }
+    for (i = 0; i < 3; i += 1) { // in case OpenGL is using 4-D coordinates
+        if (coordinates_per_vertex > Z) {
+            triangle[coordinates_per_vertex * i + Z] = 0.0;
+        }
+        if (coordinates_per_vertex > W) {
+            triangle[coordinates_per_vertex * i + W] = 1.0;
+        }
+    }
+
+    glVertexPointer(coordinates_per_vertex, GL_FLOAT, 0, triangle);
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    glEnable(GL_BLEND);
+    glDisable(GL_CULL_FACE);
+
+    glDisableClientState(GL_COLOR_ARRAY);
+    glColor4f(1, 1, 1, 1.00);
+
+    glClearColor(0.00, 0.00, 0.00, 0.00);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(1.0);
+    glLineWidth(1);
+
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+    return;
+}
+
+function main_GL() {
+    "use strict";
+    var error_code;
+
+    if (GL_get_context(document, "GL_canvas") === null) {
+        alert("Failed to initialize WebGL.");
+        return;
+    }
+
+    init();
+    setInterval(display, 1000 / frames_per_second);
+    do {
+        error_code = glGetError();
+        console.log("OpenGL error status:  " + error_code);
+    } while (error_code !== GL_NO_ERROR);
+    return;
+}
