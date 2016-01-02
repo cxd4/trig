8845ad9423917a4600f0d934cbc3483f72407590
no <rj150@hotmail.com>
2016-01-02 14:32:19 -0500

    more specific cosine test fail messages

--- trig.js
+++ trig.js
@@ -269,8 +269,12 @@ function valid_triangle() {
      // c^2 = a^2 + b^2 - 2ab sin(C); given either C = 0 or C = 180 degrees
      // c^2 = a^2 + b^2 +/- 2ab (sin(0) = +1 and sin(pi) = -1)
      // c^2 = (a - b)^2 or c^2 = (a + b)^2
-        if (Math.abs(a * a + b * b - c * c) >= 2 * a * b) {
-            alert(title + "a^2 + b^2 = c^2 +/- 2ab\n|a +/- b| = c");
+        if (Math.abs(a * a + b * b - c * c) > 2 * a * b) {
+            alert(title + "a^2 + b^2 > c^2 +/- 2ab\narccos() domain too small");
+            return false;
+        }
+        if (c === Math.abs(a - b) || c === Math.abs(a + b)) {
+            alert(title + "a^2 + b^2 = c^2 +/- 2ab\n|AB +/- BC| = AC");
             return false;
         }
 
